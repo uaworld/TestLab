423b64f9ef77888a0450e9dc2b695cc7589e3c0c
uaworld <ua@onlinehome.de>
2020-10-04 18:11:07 +0200

    test

--- src/main/java/de/uaworld/testlab/main/HelloWorld.java
+++ src/main/java/de/uaworld/testlab/main/HelloWorld.java
@@ -3,7 +3,7 @@ package de.uaworld.testlab.main;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hallo zusammen!");
 		System.exit(0);
 	}
 
